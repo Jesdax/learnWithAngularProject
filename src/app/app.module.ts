796653d8df350f5520b75d29e51559dee53c68b9
augustin <augustin_kave_08@outlook.fr>
2019-05-09 18:37:07 +0200

    Two-way binding use

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { FirstCompComponent } from './first-comp/first-comp.component';
@@ -12,7 +13,8 @@ import { DeviceComponent } from './device/device.component';
     DeviceComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
