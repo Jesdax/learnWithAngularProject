35091e4fbdf344c6dfa605e36e658b63ffa93d05
augustin <augustin_kave_08@outlook.fr>
2019-05-09 22:55:01 +0200

    use pipe && important pipe | async

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {reject} from 'q';
 
 @Component({
   selector: 'app-root',
@@ -7,8 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   $isAuth = false;
-  // $test = false;
 
+  lastUpdate = new Promise(
+    (resolve, reject) => {
+        const date = new Date();
+        setTimeout(
+          () => {
+            resolve(date);
+          }, 2000
+        );
+      }
+  );
   devices = [
     {
       name: "Machine à laver",
