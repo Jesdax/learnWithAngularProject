306abfeee906db5fe337cb8f183f6505a95c41ff
augustin <augustin_kave_08@outlook.fr>
2019-05-12 21:37:55 +0200

    RxJs-compat/ Observable first step for show second in the navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import {Component} from '@angular/core';
-
+import {Component, OnInit} from '@angular/core';
+import { Observable} from 'rxjs-compat/Observable';
+import 'rxjs-compat/add/observable/interval';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {
+export class AppComponent implements OnInit {
 
+  secondes: number;
+
+  constructor() {}
+
+  ngOnInit(): void {
+    const counter = Observable.interval(1000);
+    counter.subscribe(
+      (value) => {
+        this.secondes = value;
+      },
+      (error) => {
+        console.log('Une erreur est apparu !');
+      },
+      () => {
+        console.log('Un Observable est complété !');
+      }
+    );
   }
 }
--- src/app/single-device/single-device.component.ts
+++ src/app/single-device/single-device.component.ts
@@ -14,7 +14,7 @@ export class SingleDeviceComponent implements OnInit {
   constructor(private deviceService: DeviceServices, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id = this.route.snapshot.params['id'];
+    const id = this.route.snapshot.params.id;
     this.name = this.deviceService.getDeviceById(+id).name;
     this.status = this.deviceService.getDeviceById(+id).status;
   }
